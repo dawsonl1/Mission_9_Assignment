879f3cbebc88cd1ddebf005305968fdbc360a4f9
Dawson Pitcher <dawsonlpitcher@gmail.com>
2026-03-09 20:30:46 -0600

    Add team JSON, card, and card list funcs

--- mission_9_assignment_frontend/src/App.tsx
+++ mission_9_assignment_frontend/src/App.tsx
@@ -1,11 +1,59 @@
 import './App.css';
+import data from './CollegeBasketballTeams.json';
 
-function App() {
-  const var1 = 'Hello, World';
-
+function Heading() {
   return (
     <>
-      <h1>{var1}</h1>
+      <h1>March Maddness</h1>
+      <br />
+      <p>
+        This is a website that lists out information about all the colleges in
+        NCAA Basketball.
+      </p>
+    </>
+  );
+}
+
+function TeamCard({
+  schoolName,
+  mascotName,
+  location,
+}: {
+  schoolName: string;
+  mascotName: string;
+  location: string;
+}) {
+  return (
+    <>
+      <h2>{schoolName}</h2>
+      <p>{mascotName}</p>
+      <p>{location}</p>
+    </>
+  );
+}
+
+function CardList() {
+  return (
+    <>
+      {data.teams.map((singleTeam) => {
+        const location = `${singleTeam.city}, ${singleTeam.state}`;
+        return (
+          <TeamCard
+            schoolName={singleTeam.school}
+            mascotName={singleTeam.name}
+            location={location}
+          />
+        );
+      })}
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Heading />
+      <CardList />
     </>
   );
 }
